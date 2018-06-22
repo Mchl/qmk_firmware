7858bde671059afd82f990f49f8a82dfa8d5e2aa
Michal Jarosz <michal.jarosz@stepstone.com>
2018-06-22 13:39:54 +0200

    Use MO() instead of F() to switch layers

--- keyboards/gh60/keymaps/mchl/keymap.c
+++ keyboards/gh60/keymaps/mchl/keymap.c
@@ -12,9 +12,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, ___X___, KC_ENT,
     KC_LSFT, ___X___, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ___X___, KC_RSFT,
-    F(0),    KC_LGUI, KC_LALT,                   KC_SPC,                             ___X___, KC_RALT, F(1),    KC_APP,  F(0)
+    MO(1),   KC_LGUI, KC_LALT,                   KC_SPC,                             ___X___, KC_RALT, MO(2),  KC_APP,  MO(1)
   ),
-  /* 1: f0 */
+  /* 1: arrows + movements */
   LAYOUT(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  ___X___,
     ___X___, ___X___, KC_UP,   ___X___, ___X___, ___X___, ___X___, ___X___, KC_INS,  ___X___, ___X___, KC_HOME, KC_END,  KC_PSCR,
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,                   ___X___,                            ___X___, _______, ___X___, _______, _______
   ),
 
-    /* 2: f1 */
+    /* 2: media */
   LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,
     RESET,   _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -35,8 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_MOMENTARY(1),  // to F0 overlay
-    [1] = ACTION_LAYER_MOMENTARY(2),  // to F1 overlay
+
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
